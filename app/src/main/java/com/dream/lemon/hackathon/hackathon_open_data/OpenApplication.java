116fa0d22c16ce782096f041995b951a1ac9e653
Óscar Caballero <oscarcpozas@gmail.com>
2017-10-23 15:30:47 +0200

    Downgrade gradle to stable version

---
+++ app/src/main/java/com/dream/lemon/hackathon/hackathon_open_data/OpenApplication.java
@@ -0,0 +1,4 @@
+package com.dream.lemon.hackathon.hackathon_open_data;
+
+public class OpenApplication {
+}
---
+++ app/src/main/java/com/dream/lemon/hackathon/hackathon_open_data/ui/welcome/WelcomeContract.java
@@ -0,0 +1,16 @@
+package com.dream.lemon.hackathon.hackathon_open_data.ui.welcome;
+
+public interface WelcomeContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+
+    interface Interactor<P extends Presenter> {
+
+    }
+}
